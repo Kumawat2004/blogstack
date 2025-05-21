96c4132dc80c3e15f23b4fb78a86f2b9d1b274cb
Prakash <prakashkumawat79711@gmail.com>
2025-05-21 20:05:01 +0530

    updated

--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -32,18 +32,23 @@ const Post = () => {
     <section className="posts">
       {posts.length > 0 ? (
         <div className="container posts_container">
-          {posts.map(({ _id:id, title, thumbnail, description, creator ,category, createdAt }) => (
-            <PostItem
-              key={id}
-              postID={id}
-              title={title}
-              thumbnail={thumbnail}  // Use 'thumbnail' correctly.
-              description={description}
-              category={category}
-              authorID={creator}
-              createdAt={createdAt}
-            />
-          ))}
+       {posts && posts.length > 0 ? (
+  posts.map(({ _id: id, title, thumbnail, description, creator, category, createdAt }) => (
+    <PostItem
+      key={id}
+      postID={id}
+      title={title}
+      thumbnail={thumbnail}
+      description={description}
+      category={category}
+      authorID={creator}
+      createdAt={createdAt}
+    />
+  ))
+) : (
+  <p>No posts available.</p>
+)}
+
         </div>
       ) : (
         <h2 className="center">No posts</h2>
